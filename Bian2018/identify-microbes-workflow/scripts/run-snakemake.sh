14164e05d1dc06eee8dddec52746b7b7fd7f3385
Wells Ivens Robinson <wir963@gmail.com>
2020-03-16 18:32:16 -0400

    bug fixes to get Bian2018 to run on the server

--- Bian2018/identify-microbes-workflow/scripts/run-snakemake.sh
+++ Bian2018/identify-microbes-workflow/scripts/run-snakemake.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-module load snakemake/5.5.2
+module load snakemake
 snakemake \
 --use-conda \
 --rerun-incomplete \
---cluster-config ../config/cluster.json \
+--cluster-config config/cluster.json \
 --cluster "sbatch --partition=norm,ccr --time={cluster.time} --mem={cluster.mem} --cpus-per-task={cluster.nthreads} --gres=lscratch:{cluster.gres}" \
 --jobs 100 \
 --latency-wait 60 \
