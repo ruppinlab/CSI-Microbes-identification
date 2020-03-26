8906d305bf751c0d0bd9c07c01e5bf9459398598
Welles Robinson <wir963@gmail.com>
2020-03-26 11:17:24 -0400

    add celltype information to samples

--- Chung2017/generate-metadata-workflow/src/extract_patient_samples.py
+++ Chung2017/generate-metadata-workflow/src/extract_patient_samples.py
@@ -1,10 +1,18 @@
 import pandas as pd
 
-df = pd.read_csv(snakemake.input[0])
-df = df.rename(columns={"Run": "sample", "patient_id": "patient"})
+sra_df = pd.read_csv(snakemake.input[0])
+sra_df = sra_df.rename(columns={"Run": "sample", "patient_id": "patient", "GEO_Accession (exp)": "GSM"})
+gse_df = pd.read_csv(snakemake.input[1], sep="\t", header=None).transpose()
+gse_df.columns = ["GSM", "sample_name"]
+celltype_df = pd.read_csv(snakemake.input[2], sep="\t")
+celltype_df = celltype_df.loc[celltype_df["type"] == "SC"]
+celltype_df = celltype_df.rename(columns={"sample": "sample_name", "index": "celltype1", "index2": "celltype2", "index3": "celltype3"})
+df = gse_df.merge(celltype_df, on="sample_name")
+df = df.merge(sra_df, on="GSM")
 patient_cols = ['Cell_type', 'patient', 'molecular_subtype']
 patients = df[patient_cols].drop_duplicates()
-sample_cols = ["sample", "patient"]
+sample_cols = ["sample", "patient", "sample_name", "celltype1", "celltype2", "celltype3"]
 samples = df[sample_cols]
+
 patients.to_csv(snakemake.output[0], index=False, sep="\t")
 samples.to_csv(snakemake.output[1], index=False, sep="\t")
--- Karaayvaz2018/generate-metadata-workflow/src/extract_patient_samples.py
+++ Karaayvaz2018/generate-metadata-workflow/src/extract_patient_samples.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv(snakemake.input[0])
+sra_df = pd.read_csv(snakemake.input[0])
+sra_df = sra_df.rename(columns={"Run": "sample", "GEO_Accession (exp)": "GSM"})
+celltype_df = pd.read_csv(snakemake.input[1], sep="\t")
+celltype_df.columns = ["sample_name", "celltype"]
+gse_df = pd.read_csv(snakemake.input[2], sep="\t", header=None).transpose()
+gse_df.columns = ["sample_name", "GSM"]
+df = celltype_df.merge(gse_df, on="sample_name")
+df = df.merge(sra_df, on="GSM")
 patient_cols = ["patient", "breast_cancer_subtype", "tissue"]
 patients = df[patient_cols]
-sample_cols = ["Run", "patient", "Bases"]
+sample_cols = ["sample", "patient", "sample_name", "celltype"]
 samples = df[sample_cols]
 samples = samples.rename(columns={"Run": "sample"})
 patients.to_csv(snakemake.output[0], index=False, sep="\t")
