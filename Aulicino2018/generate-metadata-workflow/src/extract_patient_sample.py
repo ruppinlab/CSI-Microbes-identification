91370c31e1418d9ddb931afb8732c62d0e1b8611
Welles Robinson <wir963@gmail.com>
2020-04-14 10:04:02 -0400

    updated Aulicino2019 to run PathSeq and have batch tag

--- Aulicino2018/generate-metadata-workflow/src/extract_patient_sample.py
+++ Aulicino2018/generate-metadata-workflow/src/extract_patient_sample.py
@@ -5,7 +5,8 @@ df = pd.read_csv(snakemake.input[0])
 
 patients = df[['time', 'status', 'infection']].drop_duplicates()
 patients = patients.reset_index(drop=True)
-# patients["old_patient"] = ["Pt{}".format(i) for i in patients.index]
+# keep old_patient id for now
+patients["old_patient"] = ["Pt{}".format(i) for i in patients.index]
 
 samples = df[['Run', 'time', 'status', 'infection', 'plate', 'well']]
 samples = samples.rename(columns={'Run': 'sample'})
@@ -14,5 +15,7 @@ samples = samples.merge(patients, on=['time', 'status', 'infection'])
 samples["patient"] = "Pt0"
 patients["patient"] = "Pt0"
 samples["infected"] = samples["status"].apply(lambda x: "uninfected" if x == "uninfected" else "infected")
+# add batch per specifications
+samples["batch"] = "B1"
 patients.drop(columns=['time', 'status', 'infection']).drop_duplicates().to_csv(snakemake.output[0], sep="\t", index=False)
 samples.to_csv(snakemake.output[1], sep="\t", index=False)
