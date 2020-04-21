465d3eea17bc4a7283cd3b48399f7f6c5207d705
Wells Ivens Robinson <wir963@gmail.com>
2020-04-21 19:37:29 -0400

    use 16 threads for Venteicher because trying to run more rules locally

--- scripts/run-Venteicher2017-microbes.sh
+++ scripts/run-Venteicher2017-microbes.sh
@@ -1,7 +1,7 @@
 cd Venteicher2017/identify-microbes-workflow
 sbatch \
 --time=7-00:00:00 \
---cpus-per-task=8 \
---mem=8g \
+--cpus-per-task=16 \
+--mem=16g \
 --partition=norm,ccr \
 scripts/run-snakemake.sh
