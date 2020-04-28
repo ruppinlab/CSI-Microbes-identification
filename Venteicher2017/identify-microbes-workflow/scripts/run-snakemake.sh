4a645fa9b6ee9cc16b1122a92171e1d41272a64c
Welles Robinson <wir963@gmail.com>
2020-04-28 11:43:24 -0400

    run commands in batches

--- Venteicher2017/identify-microbes-workflow/scripts/run-snakemake.sh
+++ Venteicher2017/identify-microbes-workflow/scripts/run-snakemake.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-
-module load snakemake
-snakemake \
---use-conda \
---nolock \
---rerun-incomplete \
---cluster-config config/cluster.json \
---cluster "sbatch --partition={cluster.partition} --time={cluster.time} --mem={cluster.mem} --cpus-per-task={cluster.nthreads} --gres=lscratch:{cluster.gres}" \
---jobs 100 \
---latency-wait 60 \
---keep-going \
---config patient=MGH42 plate=P3
---local-cores 16 all
+INPUT=scripts/data.csv
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read patient plate
+do
+  module load snakemake
+  snakemake \
+  --use-conda \
+  --nolock \
+  --rerun-incomplete \
+  --cluster-config config/cluster.json \
+  --cluster "sbatch --partition={cluster.partition} --time={cluster.time} --mem={cluster.mem} --cpus-per-task={cluster.nthreads} --gres=lscratch:{cluster.gres}" \
+  --jobs 100 \
+  --latency-wait 60 \
+  --keep-going \
+  --config patient=$patient plate=$plate
+  --local-cores 16 all
+done < $INPUT
