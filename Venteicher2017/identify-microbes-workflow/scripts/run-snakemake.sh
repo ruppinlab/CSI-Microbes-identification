2b0c7d4bd4d7832a673b77d578e60361c90e2b4a
Welles Robinson <wir963@gmail.com>
2020-04-28 11:57:35 -0400

    minor bug fix and debugging

--- Venteicher2017/identify-microbes-workflow/scripts/run-snakemake.sh
+++ Venteicher2017/identify-microbes-workflow/scripts/run-snakemake.sh
@@ -13,6 +13,6 @@ do
   --jobs 100 \
   --latency-wait 60 \
   --keep-going \
-  --config patient=$patient plate=$plate
+  --config patient=$patient plate=$plate \
   --local-cores 16 all
 done < $INPUT
