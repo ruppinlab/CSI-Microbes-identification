ee7eb6321532526192c00afaa528c0e82eee5a5f
Welles Robinson <wir963@gmail.com>
2020-04-28 12:44:18 -0400

    update to run in batch mode

--- Venteicher2017/identify-microbes-workflow/scripts/run-snakemake.sh
+++ Venteicher2017/identify-microbes-workflow/scripts/run-snakemake.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-INPUT=scripts/data.csv
-OLDIFS=$IFS
-IFS=','
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read patient plate
-do
-  echo $patient
-  echo $plate
-  module load snakemake
-  snakemake \
-  --use-conda \
-  --nolock \
-  --rerun-incomplete \
-  --cluster-config config/cluster.json \
-  --cluster "sbatch --partition={cluster.partition} --time={cluster.time} --mem={cluster.mem} --cpus-per-task={cluster.nthreads} --gres=lscratch:{cluster.gres}" \
-  --jobs 100 \
-  --latency-wait 60 \
-  --keep-going \
-  --config patient=$patient plate=$plate \
-  --local-cores 16 all
-done < $INPUT
+
+module load snakemake
+snakemake \
+--use-conda \
+--nolock \
+--rerun-incomplete \
+--cluster-config config/cluster.json \
+--cluster "sbatch --partition={cluster.partition} --time={cluster.time} --mem={cluster.mem} --cpus-per-task={cluster.nthreads} --gres=lscratch:{cluster.gres}" \
+--jobs 100 \
+--latency-wait 60 \
+--keep-going \
+--config patient=$1 plate=$2 \
+--local-cores 16 all
--- scripts/run-Venteicher2017-microbes.sh
+++ scripts/run-Venteicher2017-microbes.sh
@@ -1,7 +1,7 @@
 cd Venteicher2017/identify-microbes-workflow
 sbatch \
 --time=7-00:00:00 \
---cpus-per-task=16 \
---mem=16g \
+--cpus-per-task=2 \
+--mem=4g \
 --partition=norm,ccr \
-scripts/run-snakemake.sh
+scripts/run-many-snakejobs.sh
