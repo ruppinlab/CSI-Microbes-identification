75798d9b395dcdf5f0c3675cdba0b9d8beb11b9e
Welles Robinson <wir963@gmail.com>
2020-04-30 11:02:29 -0400

    fixed missing files and typos

--- Jerby-Arnon2018/identify-microbes-workflow/scripts/run-many-snakejobs.sh
+++ Jerby-Arnon2018/identify-microbes-workflow/scripts/run-many-snakejobs.sh
@@ -3,13 +3,13 @@ INPUT=scripts/data.csv
 OLDIFS=$IFS
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read patient plate
+while read patient
 do
   sbatch \
   --time=7-00:00:00 \
   --cpus-per-task=2 \
   --mem=4g \
   --partition=norm,ccr \
-  scripts/run-snakemake.sh $patient $plate
+  scripts/run-snakemake.sh $patient
 
 done < $INPUT
