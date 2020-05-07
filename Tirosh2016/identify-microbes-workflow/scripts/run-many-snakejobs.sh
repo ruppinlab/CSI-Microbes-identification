039038a2d7d74509fd79f6ed205c47c95bab5e1d
Wells Ivens Robinson <wir963@gmail.com>
2020-05-07 14:32:31 -0400

    bug fixes for running Tirosh2016 on the server

--- Tirosh2016/identify-microbes-workflow/scripts/run-many-snakejobs.sh
+++ Tirosh2016/identify-microbes-workflow/scripts/run-many-snakejobs.sh
@@ -3,7 +3,7 @@ INPUT=scripts/test_data.csv
 OLDIFS=$IFS
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read patient, celltype, start, stop
+while read patient celltype start stop
 do
   sbatch \
   --time=7-00:00:00 \
