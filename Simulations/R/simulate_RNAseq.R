763a5740d0ed16651a960816c1ec5db731e4f5c4
Welles Robinson <wir963@gmail.com>
2020-07-10 15:24:41 -0400

    run simulations for two more genomes and dont simulate sequencing errors

--- Simulations/R/simulate_RNAseq.R
+++ Simulations/R/simulate_RNAseq.R
@@ -1,11 +1,11 @@
 library(Rsubread)
 library(Biostrings)
 set.seed(strtoi(snakemake@wildcards[["sample"]]))
-# data/SL1344_cds_from_genomic.fa
+
 fasta = readDNAStringSet(snakemake@input[[1]])
 
 expr = matrix(1, ncol=1, nrow=length(fasta))
 
 
 simReads(transcript.file=snakemake@input[[1]], expression.levels=expr,
-  output.prefix=snakemake@params[[1]], library.size=100000)
+  output.prefix=snakemake@params[[1]], library.size=100000, simulate.sequencing.error=FALSE)
