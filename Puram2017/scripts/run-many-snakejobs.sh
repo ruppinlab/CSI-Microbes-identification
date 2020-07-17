3f645c3ef69ee52c0aa8e0b66b870d97aa9fb8e9
Welles Robinson <wir963@gmail.com>
2020-07-17 13:15:24 -0400

    added first runs for Puram2017

---
+++ Puram2017/scripts/run-many-snakejobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+INPUT=scripts/data.csv
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read batch
+do
+  sbatch \
+  --time=7-00:00:00 \
+  --cpus-per-task=2 \
+  --mem=4g \
+  --partition=norm,ccr \
+  scripts/run-snakemake.sh $batch
+
+done < $INPUT
