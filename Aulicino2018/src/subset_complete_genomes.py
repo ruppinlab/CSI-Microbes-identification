3a279ad8946d6a082739e449fc9c820f70625304
Welles Robinson <wir963@gmail.com>
2020-07-20 11:27:27 -0400

    fix structure of Aulicino2018 and add new config

---
+++ Aulicino2018/src/subset_complete_genomes.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+column_names = [
+    "assembly_accession", "bioproject", "biosample", "wgs_master",
+    "refseq_category", "taxid", "species_taxid", "organism_name",
+    "infraspecific_name", "isolate", "version_status", "assembly_level",
+    "release_type", "genome_rep", "seq_rel_date", "asm_name", "submitter",
+    "gbrs_paired_asm", "paired_asm_comp", "ftp_path", "excluded_from_refseq",
+    "relation_to_type_material"
+    ]
+
+df = pd.read_csv("data/assembly_summary.txt", sep="\t", skiprows=2, names=column_names)
+
+# only use complete genomes
+df = df.loc[df["assembly_level"] == "Complete Genome"]
+
+# in the future, exclude the genomes that are excluded from RefSeq (only 4/13737)
+# df = df.loc[df["excluded_from_refseq"].isna()]
+
+# only keep the first genome for each species
+df = df.drop_duplicates(subset="species_taxid")
+
+# write the entire file
+df.to_csv("output/complete-genome-assembly-summary.tsv", sep="\t", index=False)
+# write the ftp_path's to file so we can use for rsync command
