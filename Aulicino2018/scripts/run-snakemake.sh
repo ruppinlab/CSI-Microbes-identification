74ae269a4f64b8302f8c0a03e20c321c39eef6a7
Welles Robinson <wir963@gmail.com>
2020-09-09 10:13:03 -0400

    run Aulicino2018 batch mode

--- Aulicino2018/scripts/run-snakemake.sh
+++ Aulicino2018/scripts/run-snakemake.sh
@@ -3,10 +3,12 @@
 module load snakemake/5.13.0
 snakemake \
 --use-conda \
+--nolock \
 --rerun-incomplete \
 --cluster-config config/cluster.json \
 --cluster "sbatch --partition={cluster.partition} --time={cluster.time} --mem={cluster.mem} --cpus-per-task={cluster.nthreads} --gres=lscratch:{cluster.gres}" \
---jobs 100 \
+--jobs 10 \
 --latency-wait 60 \
 --keep-going \
+--config plate=$1 \
 --local-cores 16 all
