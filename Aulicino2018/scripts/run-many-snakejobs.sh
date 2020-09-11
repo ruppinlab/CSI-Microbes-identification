853b826910221a351f5ef0a0be514f655c75f485
Welles Robinson <wir963@gmail.com>
2020-09-11 09:51:53 -0400

    use PathSeq host-align filter

--- Aulicino2018/scripts/run-many-snakejobs.sh
+++ Aulicino2018/scripts/run-many-snakejobs.sh
@@ -6,7 +6,7 @@ IFS=','
 while read plate
 do
   sbatch \
-  --time=7-00:00:00 \
+  --time=2-00:00:00 \
   --cpus-per-task=2 \
   --mem=4g \
   --partition=norm,ccr \
--- scripts/run-Aulicino2018-microbes.sh
+++ scripts/run-Aulicino2018-microbes.sh
@@ -1,6 +1,6 @@
 cd Aulicino2018
 sbatch \
---time=7-00:00:00 \
+--time=2-00:00:00 \
 --cpus-per-task=2 \
 --mem=4g \
 --partition=norm,ccr \
