f0f77fb258f037c122855241bbfb80645f51ee54
Welles Robinson <wir963@gmail.com>
2020-09-21 12:02:47 -0400

    run Karaayvaz2018 through CSI-Microbes v0.5.0

--- Karaayvaz2018/scripts/run-snakemake.sh
+++ Karaayvaz2018/scripts/run-snakemake.sh
@@ -3,6 +3,7 @@
 module load snakemake/5.13.0
 snakemake \
 --use-conda \
+--nolock \
 --rerun-incomplete \
 --cluster-config config/cluster.json \
 --cluster "sbatch --partition={cluster.partition} --time={cluster.time} --mem={cluster.mem} --cpus-per-task={cluster.nthreads} --gres=lscratch:{cluster.gres}" \
