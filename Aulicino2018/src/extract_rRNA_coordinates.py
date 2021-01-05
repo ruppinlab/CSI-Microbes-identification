62129e3810bc812aa9d3ca699764100639e024e2
Wells Ivens Robinson <wir963@gmail.com>
2021-01-05 15:14:37 -0500

    fix bug

--- Aulicino2018/src/extract_rRNA_coordinates.py
+++ Aulicino2018/src/extract_rRNA_coordinates.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 col_names = ["seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attribute"]
 
-df = pd.read_csv("data/GCF_000006945.2_ASM694v2_genomic.gtf", sep="\t", names=col_names, comment="#")
+df = pd.read_csv(snakemake.input[0], sep="\t", names=col_names, comment="#")
 
 if snakemake.wildcards["filter"] == "rRNA":
 # let's extract all rRNA reads
     rRNA_df = df.loc[df.attribute.str.contains("gene_biotype \"rRNA\"")]
-    rRNA_df.to_csv(snakemake.output[0], columns=["seqname", "start", "stop"], sep="\t", index=False, header=False)
+    rRNA_df[["seqname", "start", "end"]].to_csv(snakemake.output[0], sep="\t", index=False, header=False)
 
 if snakemake.wildcards["filter"] == "16S":
     rRNA_16S_df = df.loc[df.attribute.str.contains("product \"16S ribosomal RNA\"")]
-    rRNA_16S_df.to_csv(snakemake.output[0], columns=["seqname", "start", "stop"], sep="\t", index=False, header=False)
+    rRNA_16S_df[["seqname", "start", "end"]].to_csv(snakemake.output[0], sep="\t", index=False, header=False)
