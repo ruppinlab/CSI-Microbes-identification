a8035224ad0e9a287f4f870e7956371473c1779f
Wells Ivens Robinson <wir963@gmail.com>
2021-01-05 11:09:51 -0500

    minor fixes to run SRPRISM

--- Aulicino2018/src/count_nreads.py
+++ Aulicino2018/src/count_nreads.py
@@ -2,9 +2,8 @@ import pandas as pd
 import pysam
 
 output = []
-
+print(snakemake.params["cells"])
 for input in snakemake.input:
-    output.append(pysam.view("-c", input))
-
+    output.append(int(pysam.view("-c", input)))
 df = pd.DataFrame(output, index=snakemake.params["cells"])
 df.to_csv(snakemake.output[0], header=False, sep="\t")
