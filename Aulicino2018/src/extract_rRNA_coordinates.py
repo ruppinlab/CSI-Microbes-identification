aecbdb4195e79481aab54dff925531a06b931fb1
Welles Robinson <wir963@gmail.com>
2021-01-05 14:20:54 -0500

    clean up code to have one rule for multiple filters

--- Aulicino2018/src/extract_rRNA_coordinates.py
+++ Aulicino2018/src/extract_rRNA_coordinates.py
@@ -6,10 +6,11 @@ col_names = ["seqname", "source", "feature", "start", "end", "score", "strand", 
 
 df = pd.read_csv("data/GCF_000006945.2_ASM694v2_genomic.gtf", sep="\t", names=col_names, comment="#")
 
+if snakemake.wildcards["filter"] == "rRNA":
 # let's extract all rRNA reads
-rRNA_df = df.loc[df.attribute.str.contains("gene_biotype \"rRNA\"")]
-rRNA_df.to_csv(snakemake.output[0], columns=["seqname", "start", "stop"], sep="\t", index=False, header=False)
+    rRNA_df = df.loc[df.attribute.str.contains("gene_biotype \"rRNA\"")]
+    rRNA_df.to_csv(snakemake.output[0], columns=["seqname", "start", "stop"], sep="\t", index=False, header=False)
 
-# let's extract all 16S rRNA reads
-rRNA_16S_df = df.loc[df.attribute.str.contains("product \"16S ribosomal RNA\"")]
-rRNA_16S_df.to_csv(snakemake.output[1], columns=["seqname", "start", "stop"], sep="\t", index=False, header=False)
+if snakemake.wildcards["filter"] == "16S":
+    rRNA_16S_df = df.loc[df.attribute.str.contains("product \"16S ribosomal RNA\"")]
+    rRNA_16S_df.to_csv(snakemake.output[0], columns=["seqname", "start", "stop"], sep="\t", index=False, header=False)
