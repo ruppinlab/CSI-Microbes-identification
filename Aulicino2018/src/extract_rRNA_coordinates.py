80db468dce615333f098d3b8a4dca1dc48cc2912
Welles Robinson <wir963@gmail.com>
2021-01-07 10:19:59 -0500

    filter by protein coding regions

--- Aulicino2018/src/extract_rRNA_coordinates.py
+++ Aulicino2018/src/extract_rRNA_coordinates.py
@@ -14,3 +14,8 @@ if snakemake.wildcards["filter"] == "rRNA":
 if snakemake.wildcards["filter"] == "16S":
     rRNA_16S_df = df.loc[df.attribute.str.contains("product \"16S ribosomal RNA\"")]
     rRNA_16S_df[["seqname", "start", "end"]].to_csv(snakemake.output[0], sep="\t", index=False, header=False)
+
+if snakemake.wildcards["filter"] == "protein_coding":
+# let's extract all rRNA reads
+    protein_coding_df = df.loc[df.attribute.str.contains("gene_biotype \"protein_coding\"")]
+    protein_coding_df[["seqname", "start", "end"]].to_csv(snakemake.output[0], sep="\t", index=False, header=False)
