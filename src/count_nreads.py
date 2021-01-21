4cf059e9ed6b4b4ee48282ddafd6910a7d8e171a
Welles Robinson <wir963@gmail.com>
2021-01-21 14:42:50 -0500

    use intersect to generate protein catalog per cell

--- src/count_nreads.py
+++ src/count_nreads.py
@@ -2,8 +2,8 @@ import pandas as pd
 import pysam
 
 output = []
-print(snakemake.params["cells"])
+print(snakemake.params)
 for input in snakemake.input:
     output.append(int(pysam.view("-c", input)))
-df = pd.DataFrame(output, index=snakemake.params["cells"])
+df = pd.DataFrame(output, index=snakemake.params)
 df.to_csv(snakemake.output[0], header=False, sep="\t")
