0ed28f3d53a1c10a2bb1301c0b967574004e10f9
Welles Robinson <wir963@gmail.com>
2021-04-14 15:00:06 -0400

    run all for test-SS2

--- test-SS2/scripts/run-snakemake.sh
+++ test-SS2/scripts/run-snakemake.sh
@@ -9,5 +9,5 @@ snakemake \
 --jobs 100 \
 --latency-wait 60 \
 --keep-going \
---group-components download_FASTQ_from_SRA=15 compress_FASTQ_File=15 run_fastp=15 \
+--group-components download_FASTQ_from_SRA=15 compress_FASTQ_File=15 run_fastp=15 split_PathSeq_BAM_by_RG=15 extract_paired_reads=15 sort_paired_reads=15 extract_unpaired_reads=15 score_PathSeq_cell_BAM=15 \
 --local-cores 4 all
