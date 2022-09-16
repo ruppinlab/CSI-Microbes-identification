011d61bcd6140f0596f7be74dcbacc95fcb3fa50
Welles Robinson <wir963@gmail.com>
2022-09-16 10:39:23 -0400

    add 10x data from Robinson2022 to process

---
+++ Robinson2022-10x/scripts/run-snakemake.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+module load snakemake
+snakemake \
+--use-conda \
+--rerun-incomplete \
+--cluster-config config/cluster.json \
+--cluster "sbatch --partition={cluster.partition} --time={cluster.time} --mem={cluster.mem} --cpus-per-task={cluster.nthreads} --gres=lscratch:{cluster.gres}" \
+--jobs 100 \
+--latency-wait 60 \
+--keep-going \
+--group-components split_PathSeq_BAM_by_CB_UB=500 PathSeqScoreSpark=500 \
+--local-cores 4 all
