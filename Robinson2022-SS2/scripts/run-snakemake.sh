8d01c86e80cf443a3eba88fb23be9861cbc50838
Welles Robinson <wir963@gmail.com>
2022-10-04 16:21:50 -0400

    add Robinson2022-SS2 directory

---
+++ Robinson2022-SS2/scripts/run-snakemake.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+module load snakemake
+snakemake \
+--use-conda \
+--nolock \
+--rerun-incomplete \
+--cluster-config config/cluster.json \
+--cluster "sbatch --partition={cluster.partition} --time={cluster.time} --mem={cluster.mem} --cpus-per-task={cluster.nthreads} --gres=lscratch:{cluster.gres}" \
+--jobs 10 \
+--latency-wait 60 \
+--keep-going \
+--group-components score_PathSeq_cell_BAM=25 extract_unpaired_reads=25 sort_paired_reads=25 extract_paired_reads=25 split_PathSeq_BAM_by_RG=25 \
+--local-cores 4 all
